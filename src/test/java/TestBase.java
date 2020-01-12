2e9ffa80937095df628c5c21b34385e43df168cd
VladKonko <konkovladik@gmail.com>
2020-01-12 18:28:17 +0200

    Tests passed

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,9 +1,9 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
+
 import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeClass;
+
 import org.testng.annotations.BeforeSuite;
 
 import java.util.concurrent.TimeUnit;
